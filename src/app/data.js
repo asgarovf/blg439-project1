b84abb24c2ab6ddf4d7e2d51d7315aa10ebd7d62
Sıla Küçüknane <silakcknn@gmail.com>
2023-11-06 12:28:00 +0300

    added new match data

--- src/app/data.js
+++ src/app/data.js
@@ -1,4 +1,6 @@
 import { match1 } from "./data/match1";
 import { match2 } from "./data/match2";
+import { match3 } from "./data/match3";
+import { match4 } from "./data/match4";
 
-export const matches = [match1, match2, match1, match1, match1, match1];
+export const matches = [match1, match2, match3, match4];
